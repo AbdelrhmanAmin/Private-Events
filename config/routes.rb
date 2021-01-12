f62c7179475a1775b39e7d65728c08fea736d392
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-13 00:35:23 +0200

    Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   resources :events
   root 'events#index'
 end
