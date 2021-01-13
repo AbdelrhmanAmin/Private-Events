030ef42f8b490cd46945f3dbbb7dacf9bd58facb
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-14 00:04:54 +0200

    Test-2

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'devise', '~> 4.7', '>= 4.7.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :events
+  devise_for :users, :controllers => { registrations: 'registrations' }
+    devise_scope :user do
+    get '/users/sign_in' => 'devise/sessions#new'
+    get '/users/sign_in' => 'devise/sessions#create'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  resources :users
+  # root "event#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
