b174e3294d6fabb8904715c1d6ad92e4cdf8c570
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-14 00:21:19 +0200

    Assosciating

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :attendees, foreign_key: :attended_event_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events, foreign_key: :creator_id       
+  has_many :events, foreign_key: :creator_id
+  has_many :attendee, class_name: "User" ,foreign_key: :attendee_id
 end
